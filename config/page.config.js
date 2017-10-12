7dd5349bd62bf8811754aa454d1cf7103feddb77
JianZG <2992265870@qq.com>
2017-10-12 12:07:47 +0800

    -

--- config/page.config.js
+++ config/page.config.js
@@ -6,62 +6,62 @@ function getTplPath (module) {
 }
 
 module.exports = [
-  // {
-  //   name: 'index',
-  //   main: './src/pages/app.vue.js',
-  //   options: {
-  //     filename: basePath + '/index/index.php',
-  //     template: getTplPath('index'),
-  //   }
-  // },
-  // {
-  //   name: 'event',
-  //   main: './src/pages/event/app.vue.js',
-  //   options: {
-  //     filename: basePath + '/event/index.php',
-  //     template: getTplPath('event'),
-  //   }
-  // },
-  // {
-  //   name: 'product',
-  //   main: './src/pages/product/app.vue.js',
-  //   options: {
-  //     filename: basePath + '/product/list.php',
-  //     template: getTplPath('product'),
-  //   }
-  // },
-  // {
-  //   name: 'rebate',
-  //   main: './src/pages/rebate/app.vue.js',
-  //   options: {
-  //     filename: basePath + '/rebate/list.php',
-  //     template: getTplPath('rebate'),
-  //   }
-  // },
-  // {
-  //   name: 'list',
-  //   main: './src/pages/list/app.vue.js',
-  //   options: {
-  //     filename: basePath + '/list/list.php',
-  //     template: getTplPath('list'),
-  //   }
-  // },
-  // {
-  //   name: 'article_all',
-  //   main: './src/pages/article/app.vue.js',
-  //   options: {
-  //     filename: basePath + '/article/list_all.php',
-  //     template: getTplPath('article'),
-  //   }
-  // },
-  // {
-  //   name: 'article_one',
-  //   main: './src/pages/article/app_one.vue.js',
-  //   options: {
-  //     filename: basePath + '/article/list_one.php',
-  //     template: getTplPath('article'),
-  //   }
-  // },
+  {
+    name: 'index',
+    main: './src/pages/app.vue.js',
+    options: {
+      filename: basePath + '/index/index.php',
+      template: getTplPath('index'),
+    }
+  },
+  {
+    name: 'event',
+    main: './src/pages/event/app.vue.js',
+    options: {
+      filename: basePath + '/event/index.php',
+      template: getTplPath('event'),
+    }
+  },
+  {
+    name: 'product',
+    main: './src/pages/product/app.vue.js',
+    options: {
+      filename: basePath + '/product/list.php',
+      template: getTplPath('product'),
+    }
+  },
+  {
+    name: 'rebate',
+    main: './src/pages/rebate/app.vue.js',
+    options: {
+      filename: basePath + '/rebate/list.php',
+      template: getTplPath('rebate'),
+    }
+  },
+  {
+    name: 'list',
+    main: './src/pages/list/app.vue.js',
+    options: {
+      filename: basePath + '/list/list.php',
+      template: getTplPath('list'),
+    }
+  },
+  {
+    name: 'article_all',
+    main: './src/pages/article/app.vue.js',
+    options: {
+      filename: basePath + '/article/list_all.php',
+      template: getTplPath('article'),
+    }
+  },
+  {
+    name: 'article_one',
+    main: './src/pages/article/app_one.vue.js',
+    options: {
+      filename: basePath + '/article/list_one.php',
+      template: getTplPath('article'),
+    }
+  },
   //极果优品
   {
     name: 'special_youpin',
