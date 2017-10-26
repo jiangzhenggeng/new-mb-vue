37c6ac4abc038fb33e1b3801e58b337685978662
JianZG <2992265870@qq.com>
2017-10-26 19:35:41 +0800

    require

--- src/pages/app.vue.js
+++ src/pages/app.vue.js
@@ -3,10 +3,10 @@ import App from './app.vue'
 import Loading from './../components/loading'
 import store from './../store'
 import router from './../router'
-import jQuery from 'jquery'
+import jQuery from 'imports-loader?$=jquery,jQuery=jquery!jquery'
 
 window.$image_root = 'http://m.jiguo.com/test/images/'
-window.$image_load_default = Vue.$image_root + '6cd46230.lazyload_default.png'
+window.$image_load_default = window.$image_root + '6cd46230.lazyload_default.png'
 window.$WIN_HEIGHT = jQuery(window).height()
 window.$WIN_WIDTH = jQuery(window).width()
 
--- src/pages/index/block_tpl/index.ejs.js
+++ src/pages/index/block_tpl/index.ejs.js
@@ -1,5 +1,8 @@
-const indexTpl = require('./index.ejs');
-module.exports = indexTpl();
+const indexTpl = require('./index.ejs')
+const mobileUtil = require('raw-loader!./../../../tool/mobileUtil')
+module.exports = indexTpl({
+  mobileUtil
+})
 
 
 
