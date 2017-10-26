fefa4fb6b1ac724ed279cbc40db3e384baba87ac
JianZG <2992265870@qq.com>
2017-10-26 20:26:20 +0800

    试用流缩略图大小调整

--- src/pages/event/components/list-card.vue
+++ src/pages/event/components/list-card.vue
@@ -18,7 +18,7 @@
           <div class="stream-img">
             <load-img
               :big-img="`http://s1.jiguo.com/${item.fileid}/640?imageView2/1/w/640/h/320/q/100`"
-              :src="`http://s1.jiguo.com/${item.fileid}/640?imageView2/1/w/50/h/60/q/30`"
+              :src="`http://s1.jiguo.com/${item.fileid}/640?imageView2/1/w/60/h/30/q/30`"
             />
             <div class="mask-cover"></div>
           </div>
